6434fc9e54f23d66892b0df1d2affe315a9dbd47
Jlms1997 <jlm26520447@gmail.com>
2022-11-08 17:26:53 -0300

    creacion del leaderboard

--- FullstackFrontEnd/index.js
+++ FullstackFrontEnd/index.js
@@ -1,145 +1,203 @@
+// HTML Elements
+const board = document.getElementById('board');
+const scoreBoard = document.getElementById('scoreBoard');
+const rankingPopup = document.getElementById('ranking'); 
+const closeRanking = document.getElementById('closeRanking')
+const slowButton = document.getElementById('slow');
+const mediumButton = document.getElementById('medium');
+const fastButton = document.getElementById('fast');
+const leaderboardButton = document.getElementById('leaderboard');
+const gameOverSign = document.getElementById('gameOver');
+const firstInfo = document.getElementById('firstInfo');
 
-//board
-var blockSize = 26;
-var row = 30;
-var col = 30;
-var board;
-var context;
-// snake head
-var snakeX=blockSize * 5;
-var snakeY=blockSize * 5;
-var velocityX = 0;
-var velocityY = 0;
-var snakeBody = [];
-//food
-var foodX; 
-var foodY;
 
-var gameOver;
-var restart = false;
-window.onload = function() {
-    board = document.getElementById("board");
-    board.height = row * blockSize;
-    board.width = col* blockSize;
-    context = board.getContext("2d"); //se usa para dibujar en el board
-    console.log("hola mundo");
-    placeFood();
-    document.addEventListener("keyup", changeDirection);
-    setInterval(update, 1000/10);
+// Game settings
+const boardSize = 10;
+var gameSpeed = 100;
+const squareTypes = {
+    emptySquare: 0,
+    snakeSquare: 1,
+    foodSquare: 2
+};
+const directions = {
+    ArrowUp: -10,
+    ArrowDown: 10,
+    ArrowRight: 1,
+    ArrowLeft: -1,
+};
+
+// Game variables
+let snake;
+let score;
+let highscore;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const drawSnake = () => {
+    snake.forEach( square => drawSquare(square, 'snakeSquare'));
 }
 
-function update() {
+// Rellena cada cuadrado del tablero
+// @params 
+// square: posicion del cuadrado,
+// type: tipo de cuadrado (emptySquare, snakeSquare, foodSquare)
+const drawSquare = (square, type) => {
+    const [ row, column ] = square.split('');
+    boardSquares[row][column] = squareTypes[type];
+    const squareElement = document.getElementById(square);
+    squareElement.setAttribute('class', `square ${type}`);
 
-    if (gameOver){
-        return;
-    }
-
-    context.fillStyle="red";
-    context.fillRect(foodX, foodY, blockSize, blockSize);
-    if ((snakeX == foodX && snakeY == foodY )|| restart == true) {
-        snakeBody.push([foodX, foodY])
-        placeFood();
-        restart=false;
-    }
-
-    for (let i = snakeBody.length-1; i > 0; i--){
-        snakeBody[i] = snakeBody[i-1];
-    }
-
-    if(snakeBody.length){
-        snakeBody[0] = [snakeX, snakeY];
-    }
-
-    context.fillStyle="lime";
-    snakeX += velocityX * blockSize;
-    snakeY += velocityY * blockSize;
-    context.fillRect(snakeX, snakeY, blockSize,blockSize);
-
-    for (let i = 0; i < snakeBody.length; i++ ){
-        context.fillRect(snakeBody[i][0],snakeBody[i][1], blockSize, blockSize );
-    }
-
-    //game over condition
-    
-    if(snakeX < 0 || snakeX > col*blockSize || snakeY < 0 || snakeY > row*blockSize){
-        gameOver = true;
-        alert(" Game Over");
-    }
-
-    for (let i = 0; i < snakeBody.length; i++){
-        if(snakeX == snakeBody[i][0] && snakeY == snakeBody [i][1]){
-            gameOver = true;
-            alert("Game Over");
+    if(type === 'emptySquare') {
+        emptySquares.push(square);
+    } else {
+        if(emptySquares.indexOf(square) !== -1) {
+            emptySquares.splice(emptySquares.indexOf(square), 1);
         }
     }
 }
 
-function placeFood() {
-    foodX = Math.floor(Math.random() * col) * blockSize;
-    foodY = Math.floor(Math.random() * row) * blockSize;
-}
+const moveSnake = () => {
+    const newSquare = String(
+        Number(snake[snake.length - 1]) + directions[direction])
+        .padStart(2, '0');
+    const [row, column] = newSquare.split('');
 
-function changeDirection(e) {
-    if (e.code == "ArrowUp" && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;
-    }
-    else if (e.code == "ArrowDown" && velocityY != -1){
-        velocityX = 0;
-        velocityY = 1;
-    }
-    else if (e.code == "ArrowLeft" && velocityX != 1){
-        velocityX = -1;
-        velocityY = 0;
-    }
-    else if (e.code == "ArrowRight" && velocityX != -1){
-        velocityX = 1;
-        velocityY = 0;
+
+    if( newSquare < 0 || 
+        newSquare > boardSize * boardSize  ||
+        (direction === 'ArrowRight' && column == 0) ||
+        (direction === 'ArrowLeft' && column == 9 ||
+        boardSquares[row][column] === squareTypes.snakeSquare) ) {
+        gameOver();
+    } else {
+        snake.push(newSquare);
+        if(boardSquares[row][column] === squareTypes.foodSquare) {
+            addFood();
+        } else {
+            const emptySquare = snake.shift();
+            drawSquare(emptySquare, 'emptySquare');
         }
+        drawSnake();
+    }
 }
-var small = document.querySelector(".small");
-var medium = document.querySelector(".medium");
-var large = document.querySelector(".large");
 
-large.addEventListener("click", ()=>{changeLevel("large")}, false );
-small.addEventListener("click", ()=>{changeLevel("small")}, false );
-medium.addEventListener("click", ()=>{changeLevel("medium")}, false );
-
-function changeLevel(levelSize) {
-    if (levelSize == "small"){
-        context.fillStyle="white";
-        context.fillRect(0, 0, col * blockSize, row * blockSize);
-        
-        col = 20;
-        row = 20;
-        
-        context.fillStyle="black";
-        context.fillRect(0, 0, col * blockSize, row * blockSize);
-    
-        restart=true;
-    }
-    else if (levelSize == "medium"){
-        context.fillStyle="white";
-        context.fillRect(0, 0, col * blockSize, row * blockSize);
-
-        col = 23;
-        row = 23;
-        
-        context.fillStyle="blue";
-        context.fillRect(0, 0, col * blockSize, row * blockSize);
-    
-        restart = true;
-    }
-    else if (levelSize == "large"){
-        context.fillStyle="white";
-        context.fillRect(0, 0, col * blockSize, row * blockSize);
-        
-        col = 30;
-        row = 30;
-        
-        context.fillStyle="brown";
-        context.fillRect(0, 0, col * blockSize, row * blockSize);
-        
-    }
-
+const addFood = () => {
+    score++;
+    updateScore();
+    createRandomFood();
 }
+
+const gameOver = () => {
+    gameOverSign.style.display = 'block';
+    clearInterval(moveInterval)
+    slowButton.disabled = false;
+    mediumButton.disabled = false;
+    fastButton.disabled = false;
+}
+
+const setDirection = newDirection => {
+    direction = newDirection;
+}
+
+const directionEvent = key => {
+    switch (key.code) {
+        case 'ArrowUp':
+            direction != 'ArrowDown' && setDirection(key.code)
+            break;
+        case 'ArrowDown':
+            direction != 'ArrowUp' && setDirection(key.code)
+            break;
+        case 'ArrowLeft':
+            direction != 'ArrowRight' && setDirection(key.code)
+            break;
+        case 'ArrowRight':
+            direction != 'ArrowLeft' && setDirection(key.code)
+            break;
+    }
+}
+
+const createRandomFood = () => {
+    const randomEmptySquare = emptySquares[Math.floor(Math.random() * emptySquares.length)];
+    drawSquare(randomEmptySquare, 'foodSquare');
+}
+
+const updateScore = () => {
+    scoreBoard.innerText = score;
+}
+
+const createBoard = () => {
+    boardSquares.forEach( (row, rowIndex) => {
+        row.forEach( (column, columnndex) => {
+            const squareValue = `${rowIndex}${columnndex}`;
+            const squareElement = document.createElement('div');
+            squareElement.setAttribute('class', 'square emptySquare');
+            squareElement.setAttribute('id', squareValue);
+            board.appendChild(squareElement);
+            emptySquares.push(squareValue);
+        })
+    })
+}
+
+const setGame = (levelSpeed) => {
+    snake = ['00', '01', '02', '03'];
+    score = 0;
+    direction = 'ArrowRight';
+    if(levelSpeed == "slow"){
+        gameSpeed = 200;
+    }
+    else if(levelSpeed == "medium"){
+        gameSpeed = 150;
+    }
+    else if(levelSpeed == "fast"){
+        gameSpeed = 100;
+    }
+    boardSquares = Array.from(Array(boardSize), () => new Array(boardSize).fill(squareTypes.emptySquare));
+    console.log(boardSquares);
+    board.innerHTML = '';
+    emptySquares = [];
+    createBoard();
+}
+
+const startGame = (levelSpeed) => {
+    setGame(levelSpeed);
+    gameOverSign.style.display = 'none';
+    slowButton.disabled = false;
+    mediumButton.disabled = false;
+    fastButton.disabled = false;
+    drawSnake();
+    updateScore();
+    createRandomFood();
+    document.addEventListener('keydown', directionEvent);
+    moveInterval = setInterval( () => moveSnake(), gameSpeed);
+}
+
+leaderboardButton.addEventListener('click', () => {
+    rankingPopup.showModal();
+    fetch('http://localhost:5000/users')
+        .then(res => res.ok ? Promise.resolve(res): Promise.reject(res))
+        .then(res => res.json())
+        .then(res => {
+            const list = document.getElementById('firstInfo')
+            for (const userInfo of res) {
+                const listItem = document.createElement('LI')
+                listItem.textContent = `${userInfo.email} - ${userInfo.highscore}`
+                list.appendChild(listItem)
+                }
+            })
+        .catch(err=> console.log('Solicitud fallida', err));
+        rankingPopup.shoe
+    }
+    
+)
+
+closeRanking.addEventListener('click', () => {
+        rankingPopup.close();
+        firstInfo.innerHTML = '';
+    }
+)
+
+slowButton.addEventListener('click', ()=>{startGame("slow")});
+mediumButton.addEventListener('click', ()=>{startGame("medium")});
+fastButton.addEventListener('click', ()=>{startGame("fast")});
